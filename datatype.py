4e713ec810ad74bc7cf5dd1287d915b120872c5d
khushichavda <kc9293777@gmail.com>
2026-01-06 08:09:30 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,29 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello")
+hello
+x=5
+print (x)
+5
+x=20.5
+print (x)
+20.5
+y=1j
+print (y)
+1j
+x=["apple","banana","cherry"]
+print (x)
+['apple', 'banana', 'cherry']
+x=("apple","banana","cherry")
+print (x)
+('apple', 'banana', 'cherry')
+('apple', 'banana', 'cherry')
+('apple', 'banana', 'cherry')
+a=true
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    a=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x=True
+print (x)
+True
