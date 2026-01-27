de2620ed06ea09c9eab2b7f799eabe95a469f720
khushichavda <kc9293777@gmail.com>
2026-01-27 08:16:35 +0530

    Add files via upload

---
+++ oparetors.py
@@ -0,0 +1,58 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> b=10
+>>> a=5
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> x=10
+>>> print(x)
+10
+>>> a=5
+>>> print(-a)
+-5
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> a=5
+>>> b=3
+>>> print(a & b)
+1
+>>> print(a / b)
+1.6666666666666667
+>>> numbers=[1,2,3,4]
+>>> print(2 in numbers)
+True
+>>> print(5 not in numbers)
+True
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> 
