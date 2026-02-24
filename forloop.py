8c75c2177b13ee26f481d11fb3da8e6ac0c8a78d
khushichavda <kc9293777@gmail.com>
2026-02-24 09:14:37 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,21 @@
+"""for i in range(1,6):
+    print(i)
+
+    for i in range(3):
+        print("hello")
+
+   #  print number from 1 to 10
+
+for i in range(1,11):
+    print(i)  
+
+       
+"""
+"""for i in range (1, 21):
+    if i % 2 == 0:"""
+
+for i in range (1,16):
+    if i % 2 != 0:
+        print(i)
+
+     
