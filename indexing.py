afe922e7c3ddb3f629694915cadecf76ba20f343
khushichavda <kc9293777@gmail.com>
2026-03-10 07:57:16 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,9 @@
+from array  import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
